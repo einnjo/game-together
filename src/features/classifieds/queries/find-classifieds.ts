2a9ab6ad77f80a8fffa9f04bbe8da13559235546
einnjo <juanjo.lenero@gmail.com>
2020-02-04 22:35:25 -0600

    feat(classifieds): Add search by title and game
    Turns out mongo has a text search feature that should be enough for now.

--- src/features/classifieds/queries/find-classifieds.ts
+++ src/features/classifieds/queries/find-classifieds.ts
@@ -11,18 +11,46 @@ export class FindClassifieds {
   ) {}
 
   async execute(params: FindClassifiedsDto): Promise<Classified[]> {
-    const classifieds = await this.classifiedRepository.find({
-      platform: params.platform,
-      continent: params.continent
-    });
-
+    const classifieds = await this.classifiedRepository.find(
+      this.queryFromFindClassifiedsDto(params)
+    );
     await this.classifiedRepository.flush();
 
     return classifieds;
   }
+
+  private queryFromFindClassifiedsDto(
+    params: FindClassifiedsDto
+  ): FindClassifiedsQuery {
+    const query: FindClassifiedsQuery = {};
+
+    // Mongo driver does not respect undefined.
+    // driver.find({continent: undefined})
+    // will not return all documents as expected.
+    // We must therefore only add a key to the query
+    // if an explicit value is specified.
+    if (params.continent !== undefined) {
+      query.continent = params.continent;
+    }
+    if (params.platform !== undefined) {
+      query.platform = params.platform;
+    }
+    if (params.titleOrGame !== undefined) {
+      query.$text = { $search: params.titleOrGame };
+    }
+
+    return query;
+  }
+}
+
+interface FindClassifiedsQuery {
+  platform?: Platform;
+  continent?: Continent;
+  $text?: { $search: string };
 }
 
 export interface FindClassifiedsDto {
-  platform: Platform;
-  continent: Continent;
+  platform?: Platform;
+  continent?: Continent;
+  titleOrGame?: string;
 }
